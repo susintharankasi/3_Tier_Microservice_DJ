19586b2560278e5932a2c05527f1042853e663ed
susintharankasi <susindharankasi@gmail.com>
2025-02-03 10:19:27 +0000

    commit 6

--- Frontend/react-frontend/src/App.js
+++ Frontend/react-frontend/src/App.js
@@ -6,18 +6,11 @@ import dockerLogo from './assets/docker-transformed.png';
 import kubLogo from './assets/kub.png';
 import UserForm from './components/UserForm';
 import UserList from './components/UserList';
-import FetchAssets from './components/FetchAssets';
-import CreateAsset from './components/CreateAsset';
-import UpdateAsset from './components/UpdateAsset';
-import AssignAsset from './components/AssignAsset';
-import DecommissionAsset from './components/DecommissionAsset';
-import ExpiringAssets from './components/ExpiringAssets';
-import AuditLogs from './components/AuditLogs';
+import Weather from './components/weather'; 
 
 function App() {
   const [users, setUsers] = useState([]);
   const apiUrl = process.env.REACT_APP_API_URL;
-  const apiUrl1 = process.env.REACT_APP_API_URL1;
   console.log("API URL:", apiUrl);
 
   useEffect(() => {
@@ -42,8 +35,8 @@ function App() {
           <ul>
             <li><Link to="/">Dashboard</Link></li>
             <li><Link to="/users">View User Data from Microservice1</Link></li>
-            <li><Link to="/assets">Asset Management Microservice2</Link></li>
-            <li><Link to="/audit-logs">Audit Logs</Link></li>
+            <li><Link to="/weather">Weather Microservice2</Link></li>
+            <li><Link to="#">Audit Logs</Link></li>
           </ul>
         </nav>
 
@@ -71,15 +64,8 @@ function App() {
                 <UserList users={users} onDelete={handleDelete} />
               </div>
             } />
-
-            {/* Asset Management Routes */}
-            <Route path="/assets" element={<FetchAssets />} />
-            <Route path="/assets/create" element={<CreateAsset />} />
-            <Route path="/assets/update/:id" element={<UpdateAsset />} />
-            <Route path="/assets/assign/:id" element={<AssignAsset />} />
-            <Route path="/assets/decommission/:id" element={<DecommissionAsset />} />
-            <Route path="/assets/expiring" element={<ExpiringAssets />} />
-            <Route path="/audit-logs" element={<AuditLogs />} />
+            {/* Weather Page Route */}
+            <Route path="/weather" element={<Weather />} />
           </Routes>
         </div>
       </div>
