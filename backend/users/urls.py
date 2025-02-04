15cd1238610dee908d63196a44797936ad76cca1
susintharankasi <susindharankasi@gmail.com>
2025-02-04 05:55:27 +0000

    commit 15

--- backend/users/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.user_list, name='user_list'),
-    path('delete/<int:id>/', views.delete_user, name='delete_user'),
-]
-
